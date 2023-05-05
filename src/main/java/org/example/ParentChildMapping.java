0f867f8e93106e48597919443a957852f05d7d6b
unknown <nguyenducbaokey@gmail.com>
2023-05-05 20:39:25 +0700

    Finish self-reference mapping

--- src/main/java/org/example/ParentChildMapping.java
+++ src/main/java/org/example/ParentChildMapping.java
@@ -12,11 +12,46 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
+import java.util.Set;
+
 public class ParentChildMapping {
     public static void main(String[] args) {
         SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
         Session session = sessionFactory.openSession();
         Transaction transaction = session.beginTransaction();
+//        save(session);
+
+        listCategories(session);
+
+        transaction.commit();
+        session.close();
+        sessionFactory.close();
+    }
+
+    private static void listCategories(Session session) {
+        Category category = session.get(Category.class, 79l);
+
+        Set<Category> childrens = category.getChildren();
+        System.out.println(category.getName());
+        for (Category child : childrens) {
+            System.out.println("--" + child.getName());
+            printChildren(child,1);
+        }
+
+    }
+
+    private static void printChildren(Category parent, int subLevel) {
+        Set<Category> children = parent.getChildren();
+        for (Category child : children) {
+            for (int i = 0; i <= subLevel; i++)
+                System.out.print("--");
+            System.out.println(child.getName());
+            printChildren(child, subLevel + 1);
+        }
+    }
+
+
+    private static void save(Session session) {
         Category electronis = new Category("Electronics");
         Category mobilePhones = new Category("MObile Phones", electronis);
         Category washingMachines = new Category("Washing machines", electronis);
@@ -35,9 +70,5 @@ public class ParentChildMapping {
         samsung.addChild(galaxy);
 
         session.save(electronis);
-
-        transaction.commit();
-        session.close();
-        sessionFactory.close();
     }
 }
